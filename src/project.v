a262928fa4b5dedeadeaf1d461d521b0f91a69c5
Gonzalo-GtzR <gonzalo.gutierrez@tec.mx>
2025-06-14 12:35:26 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,52 +1,4 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
 
-`default_nettype none
-
-module mux_8_a_1(
-  input [7:0] data_in,
-  input [2:0] selector,
-  output reg data_out
-);
-  
-  always @(*)
-    begin
-      data_out = data_in[selector]; 
-    end
-  
-  
-endmodule
-
-
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
-
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-    
-  mux_8_a_1 CORE(
-    .data_in(ui_in),
-    .selector(uio_in[2:0]),
-      .data_out(uo_out[0])
-  );
-
-
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-  
-endmodule
-/*
 module tt_um_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
@@ -67,4 +19,4 @@ module tt_um_example (
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
-*/
+
